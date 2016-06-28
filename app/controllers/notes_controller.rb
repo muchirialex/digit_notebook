9ecc4595c5a1589ed3b2bdb82b93298ebfba6304
Alex Muchiri <muchiri94@gmail.com>
2016-06-28 20:52:28 +0300

    Add Edit & Destroy methods

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -25,9 +25,16 @@ class NotesController < ApplicationController
 	end
 
 	def update
+		if @note.update(note_params)
+			redirect_to @note
+		else
+			render 'edit'
+		end	
 	end
 
 	def destroy
+		@note.destroy
+		redirect_to notes_path
 	end
 
 	private 
